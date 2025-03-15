35a916b9b1e5d851489c18cce94d1a4df1cdc78b
mahankali <vijayakumar.mahankali@gmail.com>
2025-03-15 11:37:20 -0700

    ListGroup - event handling

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,8 +1,12 @@
+// import { MouseEvent } from "react";
+
+import { useState } from "react";
+
 // PascalCasing
 function ListGroup() {
   // const items = ["one", "two", "three", "four"];
   let items = ["one", "two", "three", "four"];
-  items = [];
+  // items = []; //1
 
   // if (items.length === 0)
   //   return (
@@ -12,23 +16,52 @@ function ListGroup() {
   //     </>
   //   );
 
-  // const message = items.length === 0 ? <p>No items found</p> : null;
-  // const getMessage = () => {
+  // const message = items.length === 0 ? <p>No items found</p> : null; //2
+  // const getMessage = () => { //3
   //   return items.length === 0 ? <p>No items found</p> : null;
   // };
+  // let selectedIndex = 0; //4
+  // const handleClick = (event: MouseEvent) => {
+  //   console.log(event);
+  // };
+
+  //hook - fn allows us to tap into built functions in REACt
+  // const arr = useState(-1);
+  // arr[0] // variable (selectedIndex)
+  // arr[1] // updater function
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  // const [name,setName] = useState('');
 
   return (
     <>
       <h1>My List</h1>
       {
-        // items.length === 0 ? <p>No items found</p> : null
-        // message
-        // getMessage()
-        items.length === 0 && <p>No items found</p>
+        // items.length === 0 ? <p>No items found</p> : null //0
+        // message //2
+        // getMessage() //3
+        items.length === 0 && <p>No items found</p> //4
       }
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}> {item}</li>
+        {items.map((item, index) => (
+          <li
+            // className="list-group-item active"
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            // onClick={(event) => console.log(event, item, index)} //5
+            // onClick={handleClick} //6
+            onClick={() => {
+              //7
+              // alert(index);
+              // selectedIndex = index; // won't work
+              setSelectedIndex(index); // works
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
