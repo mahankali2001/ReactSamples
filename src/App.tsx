691254af960f907787182c1c5bfa59eeb3b370bd
mahankali <vijayakumar.mahankali@gmail.com>
2025-03-18 09:17:39 -0700

    state management samples - update state properties including variable, array, objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import "./App.css";
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
 import React, { useState } from "react";
 import Button from "./components/Button";
 import CounterButton from "./components/CounterButton";
@@ -41,10 +7,31 @@ import ListGroup from "./components/ListGroup"; // referring to a folder, compil
 import Alert from "./components/Alert";
 import { BsFillCalendarFill } from "react-icons/bs";
 import Like from "./components/Like";
+import "./App.css";
 
 // function App() {
 const App: React.FC = () => {
   let items = ["one", "two", "three", "four"];
+
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "Vijay",
+    },
+  });
+
+  const [pizza, setPizza] = useState({
+    name: "spicy pizza",
+    toppings: ["Mushroom"],
+  });
+
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "p1", quantity: 1 },
+      { id: 2, title: "p2", quantity: 1 },
+    ],
+  });
 
   function handleItemClick(item: string): void {
     console.log(item);
@@ -55,10 +42,34 @@ const App: React.FC = () => {
 
   function handleLikeClick(): void {
     console.log("clicked");
+
+    setGame({ ...game, player: { ...game.player, name: "kumar" } });
+    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
   }
 
   return (
     <div className="p-4">
+      <p>
+        {game.id} {game.player.name}
+      </p>
+
+      <p>
+        {cart.discount}
+        <ul>
+          {cart.items.map((item) => (
+            <li key={item.id}>
+              {item.title} - {item.quantity}
+            </li>
+          ))}
+        </ul>
+      </p>
+
       <Like onClick={handleLikeClick} />
 
       <BsFillCalendarFill color="red" size="40" />
